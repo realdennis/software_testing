275ca70f6b5358241cbcabcdb9860a791b3a347a
realdennis <evolon688@gmail.com>
2018-04-09 01:25:19 +0800

    Scalene Equilateral Isosceles

--- tri/tri_unittest.cpp
+++ tri/tri_unittest.cpp
@@ -1,12 +1,9 @@
 #include "gtest/gtest.h"
 #include "tri.h"
 TEST(Boundary, normal){
-	EXPECT_EQ(tri(3, 4, 5), "Scalene");
-	EXPECT_EQ(tri(1, 4, 5), "Not a triangle");
-}
 
-TEST(Boundary,robust){
-	EXPECT_EQ(tri(0, 4, 5), "Not a triangle");
 	EXPECT_EQ(tri(1, 4, 5), "Not a triangle");
 	EXPECT_EQ(tri(3, 4, 5), "Scalene");
+	EXPECT_EQ(tri(1, 1, 1), "Equilateral");
+	EXPECT_EQ(tri(3, 3, 5), "Isosceles");
 }
