9acc3902c74b4ee49de9cb5658d711ed49d4a434
realdennis <evolon688@gmail.com>
2018-04-09 02:00:26 +0800

    a==b==c

--- tri/tri_unittest.cpp
+++ tri/tri_unittest.cpp
@@ -8,10 +8,8 @@ TEST(Boundary, normal){
 }
 
 TEST(Decision_Table,strong){
-	//a>b+c
-	EXPECT_EQ(tri(10, 4, 5), "Not a triangle");
-	//b>a+c
-	EXPECT_EQ(tri(3, 9, 5), "Not a triangle");
-	//c>a+b
-	EXPECT_EQ(tri(3, 4, 8), "Not a triangle");
+	EXPECT_EQ(tri(10, 4, 5), "Not a triangle");//a>b+c
+	EXPECT_EQ(tri(3, 9, 5), "Not a triangle");//b>a+c
+	EXPECT_EQ(tri(3, 4, 8), "Not a triangle");//c>a+b
+	EXPECT_EQ(tri(100,100,100),"Equilateral");//a==b==c
 }
