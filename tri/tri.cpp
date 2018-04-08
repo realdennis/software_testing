23dcc344ae1cfc98541b14d68f8882107fc50477
realdennis <evolon688@gmail.com>
2018-04-09 01:22:19 +0800

    isAtriangle function

--- tri/tri.cpp
+++ tri/tri.cpp
@@ -2,7 +2,15 @@
 #include<string>
 using namespace std;
 
+int isATriangle(int a, int b,int c){
+	if(	a<(b+c)	&&	b<(a+c)	&&	c<(a+b)	) return 1;	
+	return 0;
+}
+
 string tri(int a,int b, int c)
 {
-	return "Not a triangle";
+	if(isATriangle(a,b,c)){
+		return "Is A triangle";
+	}
+	else return "Not a triangle";
 }
