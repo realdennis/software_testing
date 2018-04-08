5038f35796853b08c7a89dd5be48d0ff54014639
realdennis <evolon688@gmail.com>
2018-04-09 01:56:46 +0800

    decision table

--- tri/tri_unittest.cpp
+++ tri/tri_unittest.cpp
@@ -1,9 +1,17 @@
 #include "gtest/gtest.h"
 #include "tri.h"
 TEST(Boundary, normal){
-
 	EXPECT_EQ(tri(1, 4, 5), "Not a triangle");
 	EXPECT_EQ(tri(3, 4, 5), "Scalene");
 	EXPECT_EQ(tri(1, 1, 1), "Equilateral");
 	EXPECT_EQ(tri(3, 3, 5), "Isosceles");
+}
+
+TEST(Decision_Table,strong){
+	//a>b+c
+	EXPECT_EQ(tri(10, 4, 5), "Not a triangle");
+	//b>a+c
+	EXPECT_EQ(tri(3, 9, 5), "Not a triangle");
+	//c>a+b
+	EXPECT_EQ(tri(3, 4, 8), "Not a triangle");
 }
